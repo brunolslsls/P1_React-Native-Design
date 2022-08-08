b640e86ac917c712b834f3f5fb7c31ff25f465da
Bruno <brunolopessoares80@gmail.com>
2022-08-08 11:01:44 -0300

    Inicio

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Home from './src/pages/Home';
+
+
+import Usuarios from './src/pages/Usuarios';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer // NavigationContainer = Tag responsavel para criar rotas
+    >
+      <Stack.Navigator // // Stack.Navigator = responsavel de selecionar a rota que ira ser acessada
+      >
+        <Stack.Screen  // Stack.Screen = é a rota em si que sera acessada
+          name="Home" // nome da rota
+          component={Home} // Defone o arquivo que vai ser acessado
+        />
+        <Stack.Screen name="Usuarios" component={Usuarios} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/pages/Usuarios/index.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+export default function Usuarios() {
+  return (
+    <View style={styles.container}>
+      <Text> Usuarios!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
